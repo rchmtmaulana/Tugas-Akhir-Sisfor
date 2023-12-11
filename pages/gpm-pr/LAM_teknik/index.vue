c21cb4d80930738514fc24055e1bcf07cebd398b
rchmtmaulana <rachmatmaulana81669@gmail.com>
2023-12-11 22:23:17 +0800

    GPM-PR selesai

--- pages/gpm-pr/LAM_teknik/index.vue
+++ pages/gpm-pr/LAM_teknik/index.vue
@@ -1,24 +1,40 @@
 <script setup>
 import Header from "../components/halaman_utama/header.vue";
 import Footer from "../components/halaman_utama/footer.vue";
+import { ref, computed, onMounted } from 'vue';
+
+const endpoint = "http://localhost:8055/items/Gpm/5"
+const lamData = ref([]);
+
+async function getLam() {
+  const api = await fetch(endpoint)
+  const data = await api.json()
+  console.log(data.data)
+  lamData.value = data.data;
+}
+
+onMounted(() => {
+  getLam();
+})
 </script>
 
 <template>
-    <Header/>
-    <div class="slug flex w-full text-white" style="height: 120px; margin-top: 130px; width: 100%;">
-        <img src="/home.png" alt="error" class="" style="margin: 0px 0px 0px 210px; padding-top: 28px; width: 30px; height: 60px;" />
-        <p style="margin: 0px 0px 0px 10px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin;">GPM P-R > LAM Teknik</p>
-    </div>
-    <div  Style="background-color: #d3d3dd">
-        <section class="flex justify-center relative" style="height: 700px">
-            <div class="content offside max-w-screen-xl w-full bg-white border rounded-xl overflow-hidden" style="height: 750px; margin-top: -30px;">
-                <div class="pb-5">
-                <h1 class="font-bold pt-5 text-center" style="font-family: Times New Roman Thin; font-size: 40px">LAM Teknik</h1>
-                </div>
-            </div>
-        </section>
-    </div>
-    <Footer/>
+  <Header/>
+  <div class="slug flex w-full text-white" style="height: 120px; margin-top: 130px; width: 100%;">
+    <NuxtLink to="/">
+      <img src="/home.png" alt="error" style="margin: 0px 0px 0px 210px; padding-top: 28px; width: 30px; height: 60px;" />
+      <p style="margin: 0px 0px 0px 5px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin;">Halaman</p>
+    </NuxtLink>
+    <p style="margin: 0px 0px 0px 5px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin; display: flex"> > <span style="margin-left: 5px;" v-html="lamData.nama_halaman"></span></p>
+  </div>
+  <div Style="background-color: #d3d3dd">
+      <section class="flex justify-center relative">
+          <div class="content offside py-10 px-20 max-w-screen-xl w-full bg-white border rounded-xl">
+              <p class="text-lg" v-html="lamData.konten"></p>
+          </div>
+      </section>
+  </div>
+  <Footer/>
 </template>
 
 <style>
--- pages/gpm-pr/akreditasi/index.vue
+++ pages/gpm-pr/akreditasi/index.vue
@@ -1,24 +1,40 @@
 <script setup>
 import Header from "../components/halaman_utama/header.vue";
 import Footer from "../components/halaman_utama/footer.vue";
+import { ref, computed, onMounted } from 'vue';
+
+const endpoint = "http://localhost:8055/items/Gpm/4"
+const akreditasiData = ref([]);
+
+async function getAkreditasi() {
+  const api = await fetch(endpoint)
+  const data = await api.json()
+  console.log(data.data)
+  akreditasiData.value = data.data;
+}
+
+onMounted(() => {
+  getAkreditasi();
+})
 </script>
 
 <template>
-    <Header/>
-    <div class="slug flex w-full text-white" style="height: 120px; margin-top: 130px; width: 100%;">
-        <img src="/home.png" alt="error" class="" style="margin: 0px 0px 0px 210px; padding-top: 28px; width: 30px; height: 60px;" />
-        <p style="margin: 0px 0px 0px 10px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin;">GPM P-R > Akreditasi</p>
-    </div>
-    <div  Style="background-color: #d3d3dd">
-        <section class="flex justify-center relative" style="height: 700px">
-            <div class="content offside max-w-screen-xl w-full bg-white border rounded-xl overflow-hidden" style="height: 750px; margin-top: -30px;">
-                <div class="pb-5">
-                <h1 class="font-bold pt-5 text-center" style="font-family: Times New Roman Thin; font-size: 40px">Akreditasi</h1>
-                </div>
-            </div>
-        </section>
-    </div>
-    <Footer/>
+  <Header/>
+  <div class="slug flex w-full text-white" style="height: 120px; margin-top: 130px; width: 100%;">
+    <NuxtLink to="/">
+      <img src="/home.png" alt="error" style="margin: 0px 0px 0px 210px; padding-top: 28px; width: 30px; height: 60px;" />
+      <p style="margin: 0px 0px 0px 5px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin;">Halaman</p>
+    </NuxtLink>
+    <p style="margin: 0px 0px 0px 5px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin; display: flex"> > <span style="margin-left: 5px;" v-html="akreditasiData.nama_halaman"></span></p>
+  </div>
+  <div Style="background-color: #d3d3dd">
+      <section class="flex justify-center relative">
+          <div class="content offside py-10 px-20 max-w-screen-xl w-full bg-white border rounded-xl">
+              <p class="text-lg" v-html="akreditasiData.konten"></p>
+          </div>
+      </section>
+  </div>
+  <Footer/>
 </template>
 
 <style>
