d0f7282e8f25bcc42097e697c8bb2981cc55db9f
rchmtmaulana <rachmatmaulana81669@gmail.com>
2023-12-11 20:45:38 +0800

    kemahasiswaan selesai

--- pages/kemahasiswaan/mahasiswa_inboundoutbound/index.vue
+++ pages/kemahasiswaan/mahasiswa_inboundoutbound/index.vue
@@ -1,23 +1,46 @@
 <script setup>
 import Header from "../components/halaman_utama/header.vue";
 import Footer from "../components/halaman_utama/footer.vue";
+import { ref, computed, onMounted } from 'vue';
+
+const endpoint = "http://localhost:8055/items/Kemahasiswaan/4"
+const inboundData = ref([]);
+
+async function getInbound() {
+  const api = await fetch(endpoint)
+  const data = await api.json()
+  console.log(data.data)
+  inboundData.value = data.data;
+}
+
+onMounted(() => {
+  getInbound();
+})
+
+const getImageUrl = (imageName) => {
+  return `http://localhost:8055/assets/${imageName}`;
+};
 </script>
 
 <template>
-    <Header/>
-    <div class="slug flex w-full text-white" style="height: 120px; margin-top: 130px; width: 100%;">
-        <p style="margin: 0px 0px 0px 10px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin;">Kemahasiswaan > Mahasiswa Inbound dan Outbound</p>
-    </div>
-    <div  Style="background-color: #d3d3dd">
-        <section class="flex justify-center relative" style="height: 700px">
-            <div class="content offside max-w-screen-xl w-full bg-white border rounded-xl overflow-hidden" style="height: 750px; margin-top: -30px;">
-                <div class="pb-5">
-                <h1 class="font-bold pt-5 text-center" style="font-family: Times New Roman Thin; font-size: 40px">Mahasiswa Inbound dan Outbound</h1>
-                </div>
+  <Header/>
+  <div class="slug flex w-full text-white" style="height: 120px; margin-top: 130px; width: 100%;">
+    <NuxtLink to="/">
+      <img src="/home.png" alt="error" style="margin: 0px 0px 0px 210px; padding-top: 28px; width: 30px; height: 60px;" />
+      <p style="margin: 0px 0px 0px 5px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin;">Halaman</p>
+    </NuxtLink>
+    <p style="margin: 0px 0px 0px 5px; padding-top: 30px; font-size: 20px; font-family: Times New Roman Thin; display: flex"> > <span style="margin-left: 5px;" v-html="inboundData.nama_halaman"></span></p>
+  </div>
+  <div Style="background-color: #d3d3dd">
+      <section class="flex justify-center relative">
+          <div class="content offside py-10 px-20 max-w-screen-xl w-full bg-white border rounded-xl">
+            <div class="flex justify-center mt-16"> 
+                <img :src="getImageUrl(inboundData.gambar)" :alt="`inbound ${inboundData.id}`" class="w-full object-cover rounded-md object-center" style="width: 1100px;">
             </div>
-        </section>
-    </div>
-    <Footer/>
+          </div>
+      </section>
+  </div>
+  <Footer/>
 </template>
 
 <style>
